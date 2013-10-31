e2e11a8c57ff1c9278cf7aebc271af1cc7eb0f30
Holmes J. <holmes2136@gmail.com>
2013-10-31 23:54:19 +0800

    Recommend  Algorithms

---
+++ Algorithms/Recommend.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Text;
+using System.Collections;
+
+
+/// <summary>
+/// Recommend 的摘要描述
+/// </summary>
+public class Recommend
+{
+	public Recommend(){}
+
+    public double ManhattanDistance(Hashtable rating1,Hashtable rating2) {
+
+        double distance = 0 ;
+
+        foreach (string key in rating1) { 
+            if(rating2.Contains(key)){
+                distance +=Math.Abs((double)rating1[key] - (double)rating2[key]);
+            }
+        
+        }
+
+        return distance;
+    }
+
+    public double MinkowskiDistance(Hashtable rating1, Hashtable rating2, double r)
+    {
+        double distance = 0;
+        bool commonRatings = false;
+
+        foreach (string key in rating1)
+        {
+            if (rating2.Contains(key))
+            {
+                distance += Math.Pow(Math.Abs((double)rating1[key] - (double)rating2[key]), r);
+                commonRatings = true;
+            }
+        }
+
+        if (commonRatings)
+            return Math.Pow(distance, 1/r);
+        else
+            return 0;
+
+    }
+
+}
